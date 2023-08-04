511e1405557d13399367422e67c7945375d4197b
caste77an <hancastellan@naver.com>
2023-08-04 16:39:28 +0900

    first commit

---
+++ blog-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: "castle",
+  description:
+    "Hello :) I'm castellan who develops blockchain, quant trader and full stack.",
+  author: "castellan",
+  siteUrl: "https://castellan.github.io/",
+  links: {
+    github: "https://github.com/devHudi",
+    linkedIn: "https://linkedin.com",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com",
+    email: "mailto:hancastellan@gmail.com",
+  },
+  utterances: {
+    repo: "caste77an/castle-blog",
+    type: "pathname",
+  },
+}
